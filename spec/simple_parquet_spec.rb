38f43847013dc52ca3122db4e08bfcbc015a98cb
Taylor Finnell <tmfinnell@gmail.com>
2018-09-26 16:51:17 -0400

    update test

--- spec/simple_parquet_spec.rb
+++ spec/simple_parquet_spec.rb
@@ -25,8 +25,17 @@ RSpec.describe SimpleParquet do
     output_io = StringIO.new
     csv = reader.read(output_io)
 
-    p csv
+    expected_csv = <<-EXPECTED_CSV
+Sausage,Taste
+hotdog,good
+bratwurst,just ok
+kielbasa,smoky
+chorizo,ariba
+liverwurst,irony
+andouille,sweet
+bologna,pasty
+    EXPECTED_CSV
 
-
+    expect(csv).to eq(expected_csv)
   end
 end
